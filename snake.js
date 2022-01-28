6ccc0e1abfef208d34904f309597430d669769ac
rdews <dewsbury@gmail.com>
2022-01-28 17:38:27 -0500

    update draw function

--- snake.js
+++ snake.js
@@ -4,8 +4,6 @@ const ctx = cvs.getContext("2d");
 function draw() {
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, cvs.width, cvs.height);
-
-    requestAnimationFrame(draw);
 }
 
-draw();
+setInterval(draw, 1000 / 5);
